bc857049d4916ebf346b327dadc9c6c5b2806d90
Robert Weidlich <RobertBWeidlich@yahoo.com>
2022-01-09 11:45:13 -0500

    More progress on tab-separated lines.

--- SmTsvToNdJson.py
+++ SmTsvToNdJson.py
@@ -1,19 +1,32 @@
 #!/bin/env python3
 import sys
+import re
 
 
 def main(tsv_file):
-    # explain structure of Sysmon file...
+    # for each event in Sysmon file, some date is in tab-separated line, other
+    # data is attribute-value pairs
     with open(tsv_file, 'r') as in_file:
         line_count = 0
+        rec_dict_info_attributes = []
+        rec_dict = {}
         for line in in_file:
             line = line.strip()
             line_count += 1
-            if line_count == 1:
-                print(f'>>>{line}<<<')
-                tabsep_fields = line.split('\t')
-                print(tabsep_fields)
+            print(len(line.strip()))
+            if len(rec_dict_info_attributes) < 1 and len(line.strip()) > 1:
+                print(f'1>>>{line}<<<')
+                rec_dict_info_attributes = line.split('\t')
+                print(rec_dict_info_attributes)
                 continue
+            elif re.match('^information', line, re.IGNORECASE):
+                # todo: handle cases where new record does NOT begin with "Information"
+                # look for tab-delimited lines??
+                vals = line.split('\t')
+                print(f'2>>>{line}<<<')
+                print(vals)
+                break
+
         pass
 
     return(None)
